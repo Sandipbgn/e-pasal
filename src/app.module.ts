ebbee8e5da6c9c729852f881713dd4a51f04b465
Sandipbgn <iamsbgn@gmail.com>
2023-03-28 13:27:55 +0545

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,30 +10,31 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     // ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '0000',
+    //   database: 'epasal',
+    //   entities: [User, Product],
+    //   synchronize: true,
+    //   logging: true
+    // }), 
+    UserModule,
+    ProductModule,
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0000',
+      host: 'dpg-cgdb65l269v52g64absg-a.singapore-postgres.render.com',
+      username: 'sbgn',
+      password: 'DGjBSztpO3zDq46UVUmY0yNhSsPfkpiM',
       database: 'epasal',
       entities: [User, Product],
       synchronize: true,
-      logging: true
-    }), UserModule,
-    ProductModule
-
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'dpg-cgdb65l269v52g64absg-a.singapore-postgres.render.com',
-    //   username: 'sbgn',
-    //   password: 'DGjBSztpO3zDq46UVUmY0yNhSsPfkpiM',
-    //   database: 'epasal',
-    //   entities: [User],
-    //   synchronize: true,
-    //   logging: true,
-    //   ssl: true
-    // })
+      logging: true,
+      ssl: true
+    })
     ,],
   controllers: [AppController],
   providers: [AppService],
